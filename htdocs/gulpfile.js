2e22445689c64fd66c920049ddbe360a8e586917
GS CloudVM <me.gsbit@hsx5.com>
2016-02-18 14:22:42 -0500

    adding smarter jade watch

--- htdocs/gulpfile.js
+++ htdocs/gulpfile.js
@@ -3,6 +3,10 @@ var usemin = require('gulp-jade-usemin');
 var cssnano = require('gulp-cssnano');
 var wiredep = require('wiredep').stream;
 var rename = require("gulp-rename");
+var jade = require('gulp-jade');
+var watch = require('gulp-watch');
+var affected = require('gulp-jade-find-affected');
+var filter = require('gulp-filter');
 
 gulp.task('usemin', function() {
   gulp.src('./app/jade/frame/working.jade')
@@ -26,4 +30,14 @@ gulp.task('minify-css', function() {
   return gulp.src('build/css/*.css')
     .pipe(cssnano())
     .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('watch-jade', function () {
+    watch('app/jade/**/*.jade')
+        .pipe(affected())
+        .pipe(filter(function (file) {
+            return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+        }))
+        .pipe(jade({pretty: true}))
+        .pipe(gulp.dest('app/preview'));
 });
